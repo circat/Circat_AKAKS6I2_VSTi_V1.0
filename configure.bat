2221fb18f71b4ee12fe736418b4c03f277897849
circat <erichlesovsky@gmail.com>
2026-03-05 01:16:03 +0100

    Official V2.0 Release: VST3 and README

--- configure.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-call "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" x64
-"C:\Program Files\CMake\bin\cmake.exe" -G "Visual Studio 17 2022" -A x64 -S . -B build
